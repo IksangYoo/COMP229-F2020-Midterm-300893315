81fac8d4a3e5206e42ac1676e5532f59ffde492f
IksangYoo <andrew4477@naver.com>
2020-10-31 05:46:53 +0900

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+//COMP229_Midterm IksangYoo 300893315
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://iksang:isis6911@books.dkifd.mongodb.net/books?retryWrites=true&w=majority"
+};
